fcec3e9394ec50d5df90ec60b98f83836bf585fd
rickydo <g3tdo@yahoo.com>
2015-03-25 09:46:32 -0500

    added thumbnail field to categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310002414) do
+ActiveRecord::Schema.define(version: 20150325144546) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "first_name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20150310002414) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "thumbnail"
   end
 
   create_table "comments", force: :cascade do |t|
