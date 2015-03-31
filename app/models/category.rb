e9e76934527377b3bde97dc5d5283b013b1de6f6
rickydo <g3tdo@yahoo.com>
2015-03-31 14:47:10 -0500

    adding category methods for photos users can see

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,32 @@ class Category < ActiveRecord::Base
 	extend FriendlyId
 	has_many :photos
 	friendly_id :name, use: [:slugged, :finders]
+
+	# pull out all the photos in a category
+
+	def category_photos
+		self.photos.pluck(:image, :clearance)
+	end
+
+	# pull out photos non users can view in that category
+
+	def non_user_category_photos
+		images = []
+		category_photos.each do |image|
+			images << image[0] if image[1] == 0
+		end
+		images
+	end
+
+	# pull out photos users can view with low clearance 
+
+	def low_user_category_photos
+		images = []
+		category_photos.each do |image|
+			images << image[0] if image[1] <= 1
+		end
+		images
+	end
 end
+
+
