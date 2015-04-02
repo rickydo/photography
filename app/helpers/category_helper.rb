b4109f6c486032e76884d2e177e2b7a28a082e14
rickydo <g3tdo@yahoo.com>
2015-04-02 10:59:43 -0500

    added print to jquery id method and have modal working now

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -5,10 +5,10 @@ module CategoryHelper
 
 			html << link_to(image_tag(low_img_url, class: "category_show"), 
 									  category_path(@category) + "/#",
-							 		  :'data-reveal-id'=>"#{high_img_url}")
+							 		  :'data-reveal-id'=>print_jquery_id(high_img_url))
 			html << content_tag(:div, image_tag(high_img_url), 
-								id: "#{high_img_url}", 
-								class: "reveal-modal", :'data-reveal aria-labelledby'=>"#{high_img_url}", 
+								id: print_jquery_id(high_img_url), 
+								class: "reveal-modal", :'data-reveal aria-labelledby'=>print_jquery_id(high_img_url), 
 								:'aria-hidden'=>"true",
 								:'role'=>"dialog" + 
 									content_tag(:a, '&#215'.html_safe, 
@@ -16,6 +16,10 @@ module CategoryHelper
 	
 		end
 		html.html_safe
+	end
+
+	def print_jquery_id(img_url)
+		/\w{2,}\W\d{3,}/.match(img_url).to_s
 	end
 
 end
